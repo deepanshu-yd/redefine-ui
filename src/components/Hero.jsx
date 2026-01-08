25ce29241f289b85b9cffb973b10687022dde8d0
deepanshu <yadavdeepanshu000@gmail.com>
2026-01-08 14:44:56 +0530

    fix bugs by coderabbit suggestions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
     const [loadedVideos, setLoadedVideos] = useState(0);
 
     const totalVideos = 3;
-    const nextVideodRef = useRef(null);
+    const nextVideoRef = useRef(null);
 
-    const handleVidLoad = () => {
+    const handleVideoLoad = () => {
         setLoadedVideos((prev) => prev + 1);
     }
 
@@ -31,34 +31,34 @@ const Hero = () => {
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
                     <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                         <video
-                            ref={nextVideodRef}
+                            ref={nextVideoRef}
                             src={getVideoSrc(upcomingVideoIndex)}
                             loop
                             muted
                             id="current-video"
                             className="size-64 origin-center scale-150 object-cover object-center"
-                            onLoadedData={handleVidLoad}
+                            onLoadedData={handleVideoLoad}
                         />
                     </div>
                 </div>
 
                 <video
-                    ref={nextVideodRef}
+                    ref={nextVideoRef}
                     src={getVideoSrc(currentIndex)}
                     loop
                     muted
-                    id="next-video"
-                    className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
-                    onLoadedData={handleVidLoad}
+                    id="current-video"
+                    className="size-64 origin-center scale-150 object-cover object-center"
+                    onLoadedData={handleVideoLoad}
                 />
 
                 <video
-                    src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-                    // autoPlay
+                    src={getVideoSrc(currentIndex)}
+                    autoPlay
                     loop
                     muted
                     className="absolute left-0 top-0 size-full object-cover object-center"
-                    onLoadedData={handleVidLoad}
+                    onLoadedData={handleVideoLoad}
                 />
             </div>
 
