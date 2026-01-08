7b84fd7fbc96422469b36dcd97379564744575b6
deepanshu <yadavdeepanshu000@gmail.com>
2026-01-08 15:36:55 +0530

    implement gsap animation and fix minor animation errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import { useRef, useState } from "react"
 import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,6 +25,30 @@ const Hero = () => {
         setCurrentIndex(upcomingVideoIndex);
     }
 
+    useGSAP(() => {
+        if(hasClicked) {
+            gsap.set('#next-video', {visibility: 'visible'});
+
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width:'100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play()
+            })
+
+            gsap.from('#current-video', {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut'
+            })
+        }
+
+    }, {dependencies: [currentIndex], revertOnUpdate: true})
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
@@ -31,7 +58,6 @@ const Hero = () => {
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
                     <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                         <video
-                            ref={nextVideoRef}
                             src={getVideoSrc(upcomingVideoIndex)}
                             loop
                             muted
@@ -47,8 +73,8 @@ const Hero = () => {
                     src={getVideoSrc(currentIndex)}
                     loop
                     muted
-                    id="current-video"
-                    className="size-64 origin-center scale-150 object-cover object-center"
+                    id="next-video"
+                    className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
                     onLoadedData={handleVideoLoad}
                 />
 
@@ -74,10 +100,15 @@ const Hero = () => {
 
                     <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Megame Layer <br /> Unleash the Play Economy </p>
 
-                    <Button />
+                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />}
+                    containerClass="!bg-yellow-300 flex-center gap-1" />
                 </div>
             </div>
         </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 text-black">
+            G<b>a</b>ming
+        </h1>
     </div>
   )
 }
